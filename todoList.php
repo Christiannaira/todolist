c31b1f496926efba8825b207af3b2ed0ba79ac70
christiannaira <christianmnaira@gmail.com>
2023-06-26 16:45:41 +0630

    added todolist

---
+++ todoList.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <main class="container m-5 p-5 border rounded shadow">
+        <h1 class="fw-bold">📋 Todo List</h1>
+
+        <form id="todoForm" class="my-3">
+
+            <input type="text" id="todoInput" class="form-control" placeholder="Add a todo item">
+
+            <button type="submit" class="btn btn-primary mt-2">Add</button>
+
+        </form>
+
+        <ul id="todoList" class="list-group">
+
+        </ul>
+
+    </main>
+
+    <script>
+
+        function handleSubmit(event) {
+
+
+
+        }
+
+
+        function fetchTodoItems() {
+
+
+
+        }
+
+    </script>
+
+</body>
+
+</html>
